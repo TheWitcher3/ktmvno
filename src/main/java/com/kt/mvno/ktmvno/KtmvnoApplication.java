543b891515fd45014370a5740b81e847be80407f
lee.younghyo <dldudgy7@gmail.com>
2020-08-31 16:00:33 +0900

    initail commit

---
+++ src/main/java/com/kt/mvno/ktmvno/KtmvnoApplication.java
@@ -0,0 +1,13 @@
+package com.kt.mvno.ktmvno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtmvnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtmvnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kt/mvno/ktmvno/KtmvnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.mvno.ktmvno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtmvnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
